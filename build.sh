9fd8731b5afab4ee0eedcfb0b7c8d27ca7023c6c
Alexey Zhokhov <alexey@zhokhov.com>
2016-01-09 16:55:18 +0800

    Added Dockerfile.

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VERSION="1.8.0-1"
+
+set -e
+
+echo "> 1. Building Debian package"
+echo ""
+docker build -f Dockerfile-build -t donbeave/nginx-on-steroids-build .
+
+echo ""
+echo "> 2. Publishing Debian package"
+echo ""
+docker run -e "BINTRAY_API_KEY=$BINTRAY_API_KEY" -ti donbeave/nginx-on-steroids-build
+
+sleep 60s
+echo ""
+echo ""
+echo "> 3. Building Docker image"
+echo ""
+docker build -t donbeave/nginx-on-steroids:$VERSION .
+
+echo ""
+echo "> 4. Publishing Docker image to Docker Hub"
+echo ""
+docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker push donbeave/nginx-on-steroids:$VERSION
